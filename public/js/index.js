a4cd9f59c57ff7b76aea53ea6cc15b4a45677f25
carlosscherer86 <carloseduscherer@gmail.com>
2018-05-05 00:02:12 -0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function()  {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Carlos',
-        text: 'Hey!'
-    });
 });
 
 socket.on('disconnect', function() {
